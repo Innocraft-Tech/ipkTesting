6997c3c4f19dc081562c1a31aa912f91caadb583
InnocraftTech <dev@innocrafttechnologies.com>
2025-02-08 19:02:36 +0530

    first commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,23 +1,24 @@
 import Flutter
 import UIKit
-import OtplessSDK
-
-@UIApplicationMain
+import GoogleMaps
+ 
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCejrgwc8YOfUgiyIav3acGZHifa1MY9wE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
   override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-if Otpless.sharedInstance.isOtplessDeeplink(url: url){
-Otpless.sharedInstance.processOtplessDeeplink(url: url)
+//if Otpless.sharedInstance.isOtplessDeeplink(url: url){
+//Otpless.sharedInstance.processOtplessDeeplink(url: url)
 	return true
-}
-	super.application(app, open: url, options: options)
-	return true
-
+//}
+//	super.application(app, open: url, options: options)
+//	return true
+//
 }
 }
